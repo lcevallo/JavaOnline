6ac0162aaa68e3229eaae8ee3d2cacfaa08cf55e
Luis Cevallos <lcevallo@gmail.com>
2017-06-02 16:27:33 -0500

    aula 4 hay que ver el siguiente video 5

---
+++ src/ImprimiendoVariables.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Windows 10 Turbo (L) on 05/05/2017.
+ */
+public class ImprimiendoVariables {
+
+    //Voce pode escribir
+    /*
+    comentario
+    de
+    muchas
+    lineas
+     */
+
+    /***
+     *  Este es el metodo main
+     *
+     * @param args parametro de entrada del metodo main
+     */
+
+    public static void main(String[] args){
+        int x =10;
+        int edadDosPiasParaRegistro=10;
+        System.out.println("La edad es: "+edadDosPiasParaRegistro);
+    }
+}
