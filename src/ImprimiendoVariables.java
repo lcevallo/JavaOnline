04d107006e0ed56e00bc2dd03ca2463389b1dcc5
TURBO-LTSB-64\Turbo LTSB 64 <lcevallo@gmail.com>
2017-06-06 23:23:03 -0500

    No avance con el video 6 mañana continuo

--- src/ImprimiendoVariables.java
+++ src/ImprimiendoVariables.java
@@ -18,8 +18,22 @@ public class ImprimiendoVariables {
      */
 
     public static void main(String[] args){
-        int x =10;
-        int edadDosPiasParaRegistro=10;
-        System.out.println("La edad es: "+edadDosPiasParaRegistro);
+
+
+    //Aula 6
+        int edades=10;
+        double salarioDouble=3000;
+        float salarioFloat=3000;
+        byte edadByte=12;
+        short edadShort=32767;
+        boolean verdadero=true;
+        boolean falso=false;
+        long numeroGrande=1000L;
+        char caracter='\u0041'; //2 bytes
+            int x =10;
+        //int edadDosPiasParaRegistro=10;
+        //System.out.println("La edad es: "+edadDosPiasParaRegistro);
+        String nombre="Gabrielito";
+        System.out.println(caracter);
     }
 }
