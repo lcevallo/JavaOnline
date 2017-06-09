42ae5c01f0d4ca18b881c52542b6d6a5352c89ef
TURBO-LTSB-64\Turbo LTSB 64 <lcevallo@gmail.com>
2017-06-08 22:31:26 -0500

    Video Aula 6 min 14 continuando mañana que sueño

---
+++ src/RelatorioPendencia.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Turbo LTSB 64 on 8/6/2017.
+ */
+public class RelatorioPendencia {
+    public static void main(String[] args) {
+        String nombre="Goku";
+        String direccion="A lado de un bosque cercano del Monte Paozu, junto a la choza";
+        String telefono="12323-2343-78989";
+
+        System.out.println("O "+nombre+" se encuentra en la direccion "+direccion+" y su telefono es: "+telefono);
+    }
+
+}
